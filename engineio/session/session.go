81aa2d7d74518d21fc5e1a4b42b97cfd9b91fd75
Som Prabh Sharma <>
2024-05-13 23:09:57 +0530

    rename remaining packages for go get to work with updated code

--- engineio/session/session.go
+++ engineio/session/session.go
@@ -8,11 +8,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/googollee/go-socket.io/logger"
 	"github.com/somprabhsharma/go-socket.io/engineio/frame"
 	"github.com/somprabhsharma/go-socket.io/engineio/packet"
 	"github.com/somprabhsharma/go-socket.io/engineio/payload"
 	"github.com/somprabhsharma/go-socket.io/engineio/transport"
+	"github.com/somprabhsharma/go-socket.io/logger"
 )
 
 // Pauser is connection which can be paused and resumes.
